5ec57d381cc68336765a74c9d7a9efc3b5a467f8
cyKim <76chanyoung@gmail.com>
2024-12-20 15:27:07 +0900

    바디없이 url기반 다운로드 api수정

--- Assets/Script/Manager/NetworkManager.cs
+++ Assets/Script/Manager/NetworkManager.cs
@@ -4,11 +4,10 @@ using System.Net;
 using System.Text;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Networking;
 using YDLib;
-using DefaultContractResolver = Newtonsoft.Json.Serialization.DefaultContractResolver;
 
 public class NetworkManager : SingletonObject<NetworkManager>
 {
@@ -18,14 +17,14 @@ public class NetworkManager : SingletonObject<NetworkManager>
 
     public UnityAction<bool> ConnectionStateChanged;
 
-    
+
     public async UniTask RequestConvert(string videoUrl)
     {
         var convertInfo = new ConvertRequest()
         {
             url = videoUrl,
         };
-        
+
         WebRequest request = WebRequest.Create($"{ConnectionConfigManager.Instance.GetBaseUrl()}/Convert");
         request.Method = "POST";
         request.ContentType = "application/json;charset=UTF-8";
@@ -34,12 +33,11 @@ public class NetworkManager : SingletonObject<NetworkManager>
 
         request.ContentLength = byteArray.Length;
         var dataStream = await request.GetRequestStreamAsync();
-        
+
         dataStream.WriteAsync(byteArray, 0, byteArray.Length).GetAwaiter().GetResult();
         dataStream.Close();
 
         var response = await request.GetResponseAsync();
-        
     }
 
     public async UniTask<List<FileItem>> RequestList()
@@ -50,7 +48,7 @@ public class NetworkManager : SingletonObject<NetworkManager>
         var emptyStringBytes = Encoding.UTF8.GetBytes("");
         request.ContentLength = emptyStringBytes.Length;
         var dataStream = await request.GetRequestStreamAsync();
-        
+
         dataStream.WriteAsync(emptyStringBytes, 0, emptyStringBytes.Length).GetAwaiter().GetResult();
         dataStream.Close();
 
@@ -65,46 +63,26 @@ public class NetworkManager : SingletonObject<NetworkManager>
 
         return listFileItem;
     }
-    
+
     public async UniTask<bool> RequestCheck()
     {
         WebRequest request = WebRequest.Create($"{ConnectionConfigManager.Instance.GetBaseUrl()}/Check");
-        request.Method = "GET"; 
+        request.Method = "GET";
         request.ContentType = "application/json;charset=UTF-8";
-        // var emptyStringBytes = Encoding.UTF8.GetBytes("");
-        // request.ContentLength = emptyStringBytes.Length;
-        // var dataStream = await request.GetRequestStreamAsync();
-        
-        // dataStream.WriteAsync(emptyStringBytes, 0, emptyStringBytes.Length).GetAwaiter().GetResult();
-        // dataStream.Close();
 
         using var response = (HttpWebResponse)(await request.GetResponseAsync());
 
         return response != null && response.StatusCode == HttpStatusCode.OK;
     }
 
-    
+
     public async UniTask RequestDownload(string fileName)
     {
-        var downloadRequest = new DownloadRequest()
-        {
-            name = fileName,
-            extension = ".mp3"
-        };
+        var url = $"{ConnectionConfigManager.Instance.GetBaseUrl()}/Download/{fileName}";
+        string encodedUrl = UnityWebRequest.EscapeURL(url);
         
-        WebRequest request = WebRequest.Create($"{ConnectionConfigManager.Instance.GetBaseUrl()}/Download");
-        request.Method = "GET";
-        request.ContentType = "application/json;charset=UTF-8";
-        var postData = JsonUtility.ToJson(downloadRequest);
-        var byteArray = Encoding.UTF8.GetBytes(postData);
-
-        request.ContentLength = byteArray.Length;
-        var dataStream = await request.GetRequestStreamAsync();
+        Debug.LogError(encodedUrl);
         
-        dataStream.WriteAsync(byteArray, 0, byteArray.Length).GetAwaiter().GetResult();
-        dataStream.Close();
-
-        var response = await request.GetResponseAsync();
-        
+        Application.OpenURL(encodedUrl);
     }
 }
